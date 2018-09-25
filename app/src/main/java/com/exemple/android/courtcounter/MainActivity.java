136e33120d3a7f62b56cd2734aa1e1553d8d5b96
gheorgheiulia <iulia_gh_s@yahoo.com>
2018-09-25 12:21:53 +0300

    Initial commit

---
+++ app/src/main/java/com/exemple/android/courtcounter/MainActivity.java
@@ -0,0 +1,94 @@
+package com.exemple.android.courtcounter;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity{
+    int scoreTeamA = 0;
+    int scoreTeamB = 0;
+    private View v;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        displayForTeamA(0);
+    }
+    /**
+     * Increase the score for Team A by 3 point.
+     */
+    public void addThreeForTeamA(View v) {
+        scoreTeamA = scoreTeamA + 3;
+        displayForTeamA(scoreTeamA);
+    }
+    /**
+     * Increase the score for Team A by 2 point.
+     */
+    public void addTwoForTeamA(View v) {
+        scoreTeamA = scoreTeamA + 2;
+        displayForTeamA(scoreTeamA);
+    }
+    /**
+     * Increase the score for Team A by 1 point.
+     */
+    public void addOneForTeamA(View v) {
+        scoreTeamA = scoreTeamA + 1;
+        displayForTeamA(scoreTeamA);
+    }
+    /**
+     * Displays the given score for Team A.
+     */
+    public void displayForTeamA(int score) {
+        TextView scoreView = (TextView) findViewById(R.id.team_a_score);
+        scoreView.setText(String.valueOf(score));
+    }
+
+    /**
+     * Displays the given score for Team B.
+     */
+    public void displayForTeamB(int score) {
+        TextView scoreView = (TextView) findViewById(R.id.team_b_score);
+        scoreView.setText(String.valueOf(score));
+    }
+    /**
+     * Displays the score for both teams back to 0.
+     */
+
+    /**
+     * Increase the score for Team B by 3 point.
+     */
+    public void addThreeForTeamB (View v){
+        scoreTeamB = scoreTeamB + 3;
+        displayForTeamB(scoreTeamB);
+    }
+    /**
+     * Increase the score for Team B by 2 point.
+     */
+    public void addTwoForTeamB (View v){
+        scoreTeamB = scoreTeamB + 2;
+        displayForTeamB(scoreTeamB);
+    }
+    /**
+     * Increase the score for Team B by 1 point.
+     */
+    public void addOneForTeamB (View v){
+        scoreTeamB = scoreTeamB + 1;
+        displayForTeamB(scoreTeamB);
+    }
+    /**
+     * Displays the score for both teams back to 0.
+     */
+
+    public void resetScore (View v) {
+        scoreTeamA = 0;
+        scoreTeamB = 0;
+        displayForTeamA(0);
+        displayForTeamB(0);
+    }
+
+}
